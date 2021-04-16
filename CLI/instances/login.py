11a40472ed6389f44aa1cc3da0fa4cce3b438e4d
psbhatia <psbhatia@edu.uwaterloo.ca>
2021-04-16 18:12:11 -0400

    sql connection now using the configuration file

--- CLI/instances/login.py
+++ CLI/instances/login.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 import hashlib
+import configparser
 import os
 
 '''
@@ -10,24 +11,30 @@ class Login:
     def __init__(self):
         self.connection = None
         self.cursor = None
+        self.config = None
 
     def initialize_db(self):
-        db_connection = os.getenv('DB_CONNECTION')
-        print(db_connection)
-        self.connection = self.create_connection(db_connection, 'root', 'root')
+        path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
+        self.config = configparser.ConfigParser()
+        self.config.read(os.path.join(path, 'config.ini'))
+        host_name = self.config['database']['host']
+        port_number = self.config['database']['port']
+        db_user = self.config['database']['username']
+        db_password = self.config['database']['password']
+        db_name = self.config['database']['database_name']
+        self.connection = self.create_connection(host_name, port_number, db_user, db_password, db_name)
         
         self.cursor = self.connection.cursor()
         return self.connection
     
-    def create_connection(self, host_name, user_name, user_password):
-        port_number = os.getenv('DB_PORT')
+    def create_connection(self, host_name, port_number, user_name, user_password, database_name):
         try:
             self.connection = mysql.connector.connect(
                 host=host_name,
                 port = port_number,
                 user=user_name,
                 passwd=user_password,
-                database = 'Networking'
+                database = database_name
             )
             print("Connection to MySQL DB successful")
         except Error as e:
